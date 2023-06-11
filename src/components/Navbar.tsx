eb0bd338deb3aa0722e37919952c677497141837
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-06-10 22:49:34 -0400

    adjust navbar based on screen size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,14 @@ function Navbar() {
 
     return (
         <>
-        <div>
-        <button className="md:hidden" onClick={() => setToggle(!toggle)}> Toggle </button>
-        </div>
-        <div>
-            {toggle.toString()}
-        </div>
+            <div className='flex flex-row-reverse gap-8 h-10 items-center p-3'>
+                <button className="md:hidden" onClick={() => setToggle(!toggle)}> Toggle </button>
+                <ul className='md:flex flex-row gap-8 hidden'>
+                    <li className=""> Home </li>
+                    <li className=""> About </li>
+                    <li className=""> Projects </li>
+                </ul>
+            </div>
         </>
     )
 }
