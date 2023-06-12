cd62d8e59c87d4af703e44f120a3c287f501bbbe
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-06-12 18:37:32 -0400

    completed navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,17 @@ function Navbar() {
 
     return (
         <>
-            <div className='flex flex-row-reverse gap-8 h-10 items-center p-3'>
+        <div className='flex items-center mt-3 p-2 ml-3'>
+            <div className='mr-auto text text-3xl'> Farhan Mahbub </div>
+            <div className='flex flex-row gap-8 items-center text-lg mr-3'>
                 <button className="md:hidden" onClick={() => setToggle(!toggle)}> Toggle </button>
                 <ul className='md:flex flex-row gap-8 hidden'>
-                    <li className=""> Home </li>
-                    <li className=""> About </li>
-                    <li className=""> Projects </li>
+                    <li className="hover:-translate-y-1 duration-150"> Home </li>
+                    <li className="hover:-translate-y-1 duration-150"> About Me</li>
+                    <li className="hover:-translate-y-1 duration-150"> Projects </li>
                 </ul>
             </div>
+        </div>
         </>
     )
 }
