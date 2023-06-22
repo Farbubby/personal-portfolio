a4df8b4198049b5d6a5b8f2368c79d28606f883f
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-06-21 22:33:03 -0400

    added fitting gradient color scheme to the text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,20 @@ import { useState } from "react";
 
 function Navbar() {
   const [toggle, setToggle] = useState(false);
-
-  let optionStyle = "hover:-translate-y-1 duration-150";
+  let sandcolorText = " bg-gradient-to-r from-red-500 to-orange-400 text-transparent bg-clip-text";
 
   return (
     <>
       <div className="flex items-center mt-3 p-2 ml-3 drop-shadow-glow">
-        <div className="mr-auto text text-2xl"> Farhan Mahbub </div>
+        <div className={"mr-auto text text-2xl font-bold" + sandcolorText}> Farhan Mahbub </div>
         <div className="flex flex-row gap-8 items-center text-lg mr-3">
           <button className="md:hidden" onClick={() => setToggle(!toggle)}>
             +
           </button>
-          <ul className="md:flex flex-row gap-8 hidden">
-            <li className={optionStyle}> Home </li>
-            <li className={optionStyle}> About Me </li>
-            <li className={optionStyle}> Projects </li>
+          <ul className={"md:flex flex-row gap-8 hidden font-bold" + sandcolorText}>
+            <a href="/" className="">Home</a>
+            <a href="/about" className="">About Me</a>
+            <a href="/projects" className="">Projects</a>
           </ul>
         </div>
       </div>
