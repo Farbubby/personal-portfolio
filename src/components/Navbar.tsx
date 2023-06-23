8db032ab11ea3407c38d5760d2f98d3b86adcdf5
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-06-23 00:07:50 -0400

    refactored navbar code and keep it fixed on scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,33 @@ import { useState } from "react";
 
 function Navbar() {
   const [toggle, setToggle] = useState(false);
-  let sandcolorText = " bg-gradient-to-r from-red-500 to-orange-400 text-transparent bg-clip-text";
+  let sandcolorText =
+    " bg-gradient-to-r from-red-500 to-orange-400 text-transparent bg-clip-text";
   let hoverText = " hover:text-white duration-200";
 
   return (
     <>
-      <div className="flex items-center mt-3 p-2 ml-3 drop-shadow-glow">
-        <div className={"mr-auto text text-2xl font-bold" + sandcolorText}> Farhan Mahbub </div>
+      <div className="flex mt-3 p-2 ml-3 drop-shadow-glow">
+        <div className={"mr-auto text text-2xl font-bold" + sandcolorText}>
+          Farhan Mahbub
+        </div>
         <div className="flex flex-row gap-8 items-center text-lg mr-3">
           <button className="md:hidden" onClick={() => setToggle(!toggle)}>
             +
           </button>
-          <div className={"md:flex flex-row gap-8 hidden font-bold" + sandcolorText}>
-            <a href="/" className={hoverText}>Home</a>
-            <a href="/about" className={hoverText}>About Me</a>
-            <a href="/projects" className={hoverText}>Projects</a>
+          <div
+            className={
+              "md:flex flex-row gap-8 hidden font-bold" + sandcolorText
+            }>
+            <a href="/" className={hoverText}>
+              Home
+            </a>
+            <a href="/about" className={hoverText}>
+              About Me
+            </a>
+            <a href="/projects" className={hoverText}>
+              Projects
+            </a>
           </div>
         </div>
       </div>
