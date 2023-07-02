de8e5cb0c0778d7677e197757f9360985e57b1be
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-07-02 12:09:52 -0400

    small fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,14 +19,11 @@ function Navbar() {
   return (
     <>
       <div className="flex flex-row-reverse mt-3 px-4 ml-3 drop-shadow-glow items-center h-20">
-        <div className="flex flex-row gap-8 text-lg mr-3">
+        <div className={"flex flex-row gap-8 text-lg mr-3" + sandcolorText}>
           <button className="md:hidden" onClick={() => setToggle(!toggle)}>
             +
           </button>
-          <div
-            className={
-              "md:flex flex-row gap-8 hidden font-bold" + sandcolorText
-            }>
+          <div className={"md:flex flex-row gap-8 hidden font-bold"}>
             <button onClick={() => scrollTo("home")} className={hoverText}>
               Home
             </button>
