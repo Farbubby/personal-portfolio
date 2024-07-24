7ad00bba28f00eada70dd077aaa547ccf06a7a6d
farbubby <farhan.mahbub@ucf.edu>
2024-07-24 17:43:35 -0400

    Added google analytics tag for user metrics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
